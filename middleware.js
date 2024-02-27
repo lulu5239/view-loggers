19866bf1251db060566bdce6065f1d9c3d40223f
Lulu5239 <32999164+lulu5239@users.noreply.github.com>
2024-02-27 13:14:57 +0100

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,9 +1,9 @@
 import { NextRequest, NextResponse, userAgent } from 'next/server';
 
 export async function middleware(req) {
-  const ua = userAgent(req);
-  console.log(req.url,ua)
-  if(req.url==="/mini.png"){
+  const ua = userAgent(req)?.ua;
+  const page = req.url.split("/").slice(-1)[0]
+  if(page==="mini.png"){
     // Nothing...
   }else if (true) {
     // Log that the image has been loaded
