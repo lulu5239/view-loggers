e8910d6ed3b0406daa90331d13ec61572482df82
obfuscated-loop <107592590+obfuscated-loop@users.noreply.github.com>
2024-03-13 23:28:18 +0000

    Format code

--- middleware.js
+++ middleware.js
@@ -2,24 +2,28 @@ import { NextRequest, NextResponse, userAgent } from 'next/server';
 
 const webhook = process.env.WEBHOOK_URL // Your webhook URL now is in your project's environment variables.
 
-export async function middleware(req){
+export async function middleware(req) {
   const ua = userAgent(req)?.ua;
-  const source = ["Mozilla/5.0 (compatible; Discordbot/","Twitterbot/"].find(u=>ua?.startsWith(u))
+  const source = ["Mozilla/5.0 (compatible; Discordbot/", "Twitterbot/"].find(u => ua?.startsWith(u))
   const page = req.url.split("/").slice(-1)[0]
-  await fetch(webhook,{body:JSON.stringify({
-    embeds:[{
-      title:"Triggered view-logger",
-      description:(source ? "Source user-agent: "+ua : "It was loaded an user (or an user on Discord)."),
-      footer:{
-        text:"Requested page: "+page.slice(0,500),
-      },
-    }],
-  }),headers:{"content-type":"application/json"},method:"POST"})
-  if(source){
+  
+  await fetch(webhook, {
+    body: JSON.stringify({
+      embeds: [{
+        title: "Triggered view-logger",
+        description: (source ? "Source user-agent: " + ua : "It was loaded an user (or an user on Discord)."),
+        footer: {
+          text: "Requested page: " + page.slice(0, 500),
+        },
+      }],
+    }), headers: { "content-type": "application/json" }, method: "POST"
+  })
+
+  if (source) {
     // Return the image.
-    return NextResponse.rewrite(new URL("/mini.png",req.url))
-  }else{
+    return NextResponse.rewrite(new URL("/mini.png", req.url))
+  } else {
     // Make a message for whoever takes the risk to directly click.
-    return NextResponse.rewrite(new URL("/page.html",req.url));
+    return NextResponse.rewrite(new URL("/page.html", req.url));
   }
 }
